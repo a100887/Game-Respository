90fe5fac66b7c7a75ba8833d6c2300e87adf33a2
Silvan Vella <Silvan Vella>
2017-11-09 16:22:33 +0100

    - Added a background image - Changed game resolution - Added a ball and paddle objects to the scene - Attached physics components to the game objects - Added a lose collider border

---
+++ Brick Breaker/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    static MusicPlayerScript instance = null;
+    //static bool availablePlayer = false;
+
+	// Use this for initialization
+	void Start () {
+
+        //if (availablePlayer == false)
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+            //availablePlayer = true;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+            print("Duplicate Music Player is self destructing!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NumberWizardUI/Assets/Scripts/NumberWizardScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizardScript : MonoBehaviour {
+
+    public Text myGuess;
+
+    int min = 0;
+    int max = 0;
+    int guess = 0;
+    int maxGuesses = 5;
+
+    // Use this for initialization
+    void Start () {
+
+        StartGame();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void StartGame()
+    {
+
+        min = 1;
+        max = 1000;
+        //guess = 500;
+
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        if (maxGuesses == 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+        else {
+            guess = Random.Range(min, (max + 1));
+            myGuess.text = guess + "?";
+            maxGuesses--;
+        }
+    }
+
+    public void GuessHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+        
+    }
+
+    
+    
+}
